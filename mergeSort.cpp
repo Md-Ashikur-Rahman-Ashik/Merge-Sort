fd048b74078e37b0d74996a61047e5e697b2cb12
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-10-01 22:59:30 +0600

    Implemented merge sort

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -1,6 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void conquer(int initialArray[], int leftIndex, int mid, int rightIndex)
+{
+    int firstArraySize = mid - leftIndex + 1;
+    int firstArray[firstArraySize];
+
+    int initialPointer = leftIndex;
+
+    for (int i = 0; i < firstArraySize; i++)
+    {
+        firstArray[i] = initialArray[initialPointer];
+        initialPointer++;
+    }
+
+    int secondArraySize = rightIndex - mid;
+    int secondArray[secondArraySize];
+
+    initialPointer = mid + 1;
+    for (int i = 0; i < secondArraySize; i++)
+    {
+        secondArray[i] = initialArray[initialPointer];
+        initialPointer++;
+    }
+
+    int firstPointer = 0, secondPointer = 0;
+    int finalPointer = leftIndex;
+
+    while (firstPointer < firstArraySize && secondPointer < secondArraySize)
+    {
+        if (firstPointer < firstArraySize && secondPointer < secondArraySize && firstArray[firstPointer] <= secondArray[secondPointer])
+        {
+            initialArray[finalPointer] = firstArray[firstPointer];
+            firstPointer++;
+            finalPointer++;
+        }
+        else
+        {
+            initialArray[finalPointer] = secondArray[secondPointer];
+            secondPointer++;
+            finalPointer++;
+        }
+    }
+
+    while (firstPointer < firstArraySize)
+    {
+        initialArray[finalPointer] = firstArray[firstPointer];
+        firstPointer++;
+        finalPointer++;
+    }
+
+    while (secondPointer < secondArraySize)
+    {
+        initialArray[finalPointer] = secondArray[secondPointer];
+        secondPointer++;
+        finalPointer++;
+    }
+}
+
 void divide(int integerArray[], int leftIndex, int rightIndex)
 {
     if (leftIndex >= rightIndex)
@@ -14,10 +71,6 @@ void divide(int integerArray[], int leftIndex, int rightIndex)
     conquer(integerArray, leftIndex, mid, rightIndex);
 }
 
-void conquer(int integerArray[], int leftIndex, int mid, int rightIndex)
-{
-}
-
 int main()
 {
     int arraySize;
@@ -29,6 +82,11 @@ int main()
     }
 
     divide(integerArray, 0, arraySize - 1);
+
+    for (int i = 0; i < arraySize; i++)
+    {
+        cout << integerArray[i] << " ";
+    }
 
     return 0;
 }
